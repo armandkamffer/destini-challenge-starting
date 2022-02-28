e7b22111fee801908e3954339e370c6bd2eb24c1
Armand Kamffer <armand.kamffer@entelect.co.za>
2022-02-28 12:52:41 +0200

    Implement navigation of story questions

--- lib/story.dart
+++ lib/story.dart
@@ -1,5 +1,15 @@
-//TODO: Step 2 - Create a new class called Story.
+class Story {
+  String storyTitle;
+  String choiceOne;
+  String choiceTwo;
+  int choiceOneStoryNumber;
+  int choiceTwoStoryNumber;
 
-//TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
-
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+  Story({String storyTitle, String choiceOne, String choiceTwo, int choiceOneStoryNumber, int choiceTwoStoryNumber}) {
+    this.storyTitle = storyTitle;
+    this.choiceOne = choiceOne;
+    this.choiceTwo = choiceTwo;
+    this.choiceOneStoryNumber = choiceOneStoryNumber;
+    this.choiceTwoStoryNumber = choiceTwoStoryNumber;
+  }
+}
